c60465e7d56e70e88647a07b55590e0423c5564c
r3d5un <r3d5un@pm.me>
2024-02-14 20:58:28 +0100

    Print out all units from downloaded zip

--- cmd/brreg-units/main.go
+++ cmd/brreg-units/main.go
@@ -1,9 +1,42 @@
 package main
 
 import (
+	"bytes"
+	"compress/gzip"
+	"encoding/json"
 	"fmt"
+	"io"
+	"os"
+
+	"github.com/r3d5un/Airflow/internal/brreg/units"
 )
 
+type UnitSlice []units.Unit
+
 func main() {
-	fmt.Println("Hello, world!")
+	data, err := os.ReadFile("./enheter_alle.json.gz")
+	if err != nil {
+		fmt.Println("Error reading file", err)
+		os.Exit(1)
+	}
+
+	reader := bytes.NewReader(data)
+	gzReader, err := gzip.NewReader(reader)
+	if err != nil {
+		fmt.Println("Error creating gzip reader", err)
+		os.Exit(1)
+	}
+
+	unzippedData, err := io.ReadAll(gzReader)
+	if err != nil {
+		fmt.Println("Error reading unzipped data", err)
+		os.Exit(1)
+	}
+
+	var units UnitSlice
+	err = json.Unmarshal(unzippedData, &units)
+
+	for _, unit := range units {
+		fmt.Println(unit.Navn)
+	}
 }
