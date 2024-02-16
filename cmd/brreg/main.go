4c987b662bd88dd96a044801221d718514a63508
r3d5un <r3d5un@pm.me>
2024-02-16 21:26:22 +0100

    Implement subunits and CLI functionality

--- cmd/brreg/main.go
+++ cmd/brreg/main.go
@@ -6,6 +6,7 @@ import (
 	"context"
 	"database/sql"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io"
 	"log/slog"
@@ -19,40 +20,96 @@ import (
 )
 
 type UnitSlice []brreg.Unit
+type SubUnitSlice []brreg.SubUnit
 
 func main() {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	slog.SetDefault(logger)
 
-	logger.Info("reading file")
-	unzippedData, err := read("./enheter_alle.json.gz")
-	if err != nil {
-		fmt.Println("Error reading file", err)
-		os.Exit(1)
-	}
-
-	logger.Info("marshalling data")
-	var units UnitSlice
-	err = json.Unmarshal(unzippedData, &units)
+	dataset := flag.String(
+		"dataset",
+		"units",
+		"The desired dataset. Choice between units and subunits",
+	)
+	dsn := flag.String(
+		"dsn",
+		"postgres://postgres:postgres@localhost:5432/warehouse?sslmode=disable",
+		"Database DSN (postgres://user:password@localhost:5432/db)",
+	)
+	workers := flag.Int(
+		"workers",
+		25,
+		"Sets DB connections and number of concurrent processes",
+	)
+	flag.Parse()
 
 	logger.Info("opening db")
-	dsn := "postgres://postgres:postgres@localhost:5432/warehouse?sslmode=disable"
-	db, err := openDB(dsn)
+	db, err := openDB(*dsn, *workers)
 	if err != nil {
-		fmt.Println("Error opening db", err)
+		logger.Error("Error opening db", err)
 		os.Exit(1)
 	}
 	defer db.Close()
-
 	queryTimeout := time.Duration(10) * time.Second
-
 	models := data.NewModels(db, logger, &queryTimeout)
 
+	switch *dataset {
+	case "units":
+		logger.Info("reading file")
+		unzippedUnits, err := read("./enheter_alle.json.gz")
+		if err != nil {
+			fmt.Println("Error reading file", err)
+			os.Exit(1)
+		}
+
+		var units UnitSlice
+		err = json.Unmarshal(unzippedUnits, &units)
+		ingestUnits(units, *workers, &models, logger)
+	case "subunits":
+		unzippedSubUnits, err := read("./underenheter_alle.json.gz")
+		if err != nil {
+			fmt.Println("Error reading file", err)
+			os.Exit(1)
+		}
+
+		var subunits SubUnitSlice
+		err = json.Unmarshal(unzippedSubUnits, &subunits)
+
+		ingestSubUnits(subunits, *workers, &models, logger)
+	default:
+		logger.Error("no valid dataset selected")
+		os.Exit(1)
+	}
+
+	logger.Info("done")
+}
+
+func read(path string) (d []byte, err error) {
+	file, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	reader := bytes.NewReader(file)
+	gzReader, err := gzip.NewReader(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	d, err = io.ReadAll(gzReader)
+	if err != nil {
+		return nil, err
+	}
+
+	return d, nil
+}
+
+func ingestUnits(units UnitSlice, workers int, models *data.Models, logger *slog.Logger) {
 	logger.Info("upserting units")
 	var wg sync.WaitGroup
 	unitChan := make(chan data.Unit, len(units))
 
-	for i := 0; i < 25; i++ {
+	for i := 0; i < workers; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
@@ -82,34 +139,50 @@ func main() {
 	logger.Info("all units upserted")
 }
 
-func read(path string) (d []byte, err error) {
-	file, err := os.ReadFile(path)
-	if err != nil {
-		return nil, err
+func ingestSubUnits(subunits SubUnitSlice, workers int, models *data.Models, logger *slog.Logger) {
+	logger.Info("upserting subunits")
+	var wg sync.WaitGroup
+	subUnitChan := make(chan data.SubUnit, len(subunits))
+
+	for i := 0; i < workers; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for su := range subUnitChan {
+				result, err := models.SubUnits.Upsert(context.Background(), &su)
+				if err != nil {
+					logger.Error("error upserting subunit", "error", err)
+					return
+				}
+
+				logger.Info("subunit upserted", "id", result.ID)
+			}
+		}()
 	}
 
-	reader := bytes.NewReader(file)
-	gzReader, err := gzip.NewReader(reader)
-	if err != nil {
-		return nil, err
+	for _, su := range subunits {
+		record := data.SubUnit{
+			ID:           su.OrganisasjonsNummer,
+			ParentID:     su.OverordnetEnhet,
+			BRREGSubUnit: &su,
+		}
+		subUnitChan <- record
 	}
+	close(subUnitChan)
 
-	d, err = io.ReadAll(gzReader)
-	if err != nil {
-		return nil, err
-	}
+	wg.Wait()
 
-	return d, nil
+	logger.Info("all subunits upserted")
 }
 
-func openDB(dsn string) (*sql.DB, error) {
+func openDB(dsn string, workers int) (*sql.DB, error) {
 	db, err := sql.Open("pgx", dsn)
 	if err != nil {
 		return nil, err
 	}
 
-	db.SetMaxOpenConns(25)
-	db.SetMaxIdleConns(25)
+	db.SetMaxOpenConns(workers)
+	db.SetMaxIdleConns(workers)
 
 	duration, err := time.ParseDuration("5m")
 	if err != nil {
