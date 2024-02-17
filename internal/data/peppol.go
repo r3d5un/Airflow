9a3b02daf39ad0aefa94c655e062314937cc33aa
r3d5un <r3d5un@pm.me>
2024-02-17 14:23:02 +0100

    Add Get method to PEPPOL data structs

--- internal/data/peppol.go
+++ internal/data/peppol.go
@@ -3,6 +3,8 @@ package data
 import (
 	"context"
 	"database/sql"
+	"encoding/json"
+	"errors"
 	"log/slog"
 	"time"
 
@@ -21,4 +23,39 @@ type PeppolBusinessCardModel struct {
 	Timeout *time.Duration
 	DB      *sql.DB
 	Logger  *slog.Logger
+}
+
+func (m *PeppolBusinessCardModel) Get(ctx context.Context, id string) (*PeppolBusinessCard, error) {
+	stmt := `SELECT id, name, countrycode, last_updated, business_cards
+FROM peppol_business_cards
+WHERE id = $1;`
+
+	qCtx, cancel := context.WithTimeout(ctx, *m.Timeout)
+	defer cancel()
+
+	var bc PeppolBusinessCard
+	var jsonBc []byte
+
+	m.Logger.InfoContext(qCtx, "querying peppol business card", "query", stmt, "id", id)
+	row := m.DB.QueryRowContext(ctx, stmt, id)
+	err := row.Scan(&bc.ID, &bc.Name, &bc.CountryCode, &bc.LastUpdated, jsonBc)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			m.Logger.InfoContext(ctx, "no records found", "query", stmt, "id", id)
+			return nil, ErrNoRecord
+		} else {
+			m.Logger.InfoContext(ctx, "unable to query business card", "query", stmt, "id", id)
+			return nil, err
+		}
+	}
+	m.Logger.InfoContext(ctx, "data retrieved")
+
+	err = json.Unmarshal(jsonBc, &bc.PeppolBusinessCard)
+	if err != nil {
+		m.Logger.ErrorContext(ctx, "error unmarshaling peppol_business_card", "error", err)
+		return nil, err
+	}
+
+	return &bc, nil
+
 }
